8366c4493c5721ce6df0da3caeac089821c14b4d
Jhenifer Candido <jhenifercristinacandido@gmail.com>
2024-06-25 14:08:40 -0300

    js site

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -4,25 +4,25 @@ var diaSem = agora.getDay()
 
 switch (diaSem) { //so funciona com numeros inteiros  e situações pontuais..
     case 0:
-        console.log('Domingo')
+        console.log('Hoje é Domingo')
         break
     case 1:
-        console.log('segunda')
+        console.log(' Hoje é segunda')
         break
     case 2:
-            console.log('Terça')
+            console.log('Hoje é Terça')
             break
     case 3:
-        console.log('Quarta')
+        console.log('Hoje é Quarta')
         break
     case 4:
-            console.log('Quinta')
+            console.log('Hoje é Quinta')
             break
     case 5:
-        console.log('Sexta')
+        console.log('Hoje é Sexta')
         break
     case 6:
-        console.log('Sábado')
+        console.log('Hoje é Sábado')
         break
     default:
         console.log('[ERRO] Dia inválido')
---
+++ site/script.js
@@ -0,0 +1,27 @@
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
+
+    alert('Mensagem enviada com sucesso!');
+    this.reset();
+});
